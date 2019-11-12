64f6b9ed23e31c284622b16ea44076678793d3e7
Serena Chan <serena.cx13@gmail.com>
2019-11-12 13:54:33 -0500

    removed env

--- databaes-backend/env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/serenachan/Documents/1920-fall/cs326/final-project/databaes-backend/env/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
