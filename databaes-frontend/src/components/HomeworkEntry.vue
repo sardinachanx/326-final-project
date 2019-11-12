e2a4f4fdf5e36b6a5a9f9d5a824156499a841afe
gbear605 <garrisontaylor@gmail.com>
2019-11-12 14:02:01 -0500

    Add basic homework todo list

---
+++ databaes-frontend/src/components/HomeworkEntry.vue
@@ -0,0 +1,29 @@
+<template>
+  <li class="homework_entry">
+    <button v-on:click="$emit('removeHomework', id)" type="button">
+      Complete
+    </button>
+    {{ name }}
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'AddHomeworkForm',
+  props: {
+    name: String,
+    id: Number
+  },
+  data: function () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.homework_entry {
+  display: block;
+}
+</style>
