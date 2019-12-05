7c2ba4623fe32bdbb92ec48f3d683124bd12eb9a
Serena Chan <serenachan@1x-nat-vl930-172-30-67-203.wireless.umass.edu>
2019-12-05 17:06:10 -0500

    Added assignment stats

--- databaes-backend/api/serializers.py
+++ databaes-backend/api/serializers.py
@@ -1,3 +1,4 @@
+from collections import OrderedDict
 import datetime
 from datetime import date, timedelta
 
@@ -6,7 +7,15 @@ from rest_framework import serializers
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from .models import Course, Assignment, Enrollment, DayEntry, Student
 
+# Helper methods/fields
+
 User = get_user_model()
+
+def daterange(start_date, end_date):
+    for n in range(int ((end_date - start_date).days)):
+        yield start_date + timedelta(n)
+
+# Serializers
 
 class EnrollmentSerializer(serializers.ModelSerializer):
     user = serializers.HiddenField(default=serializers.CurrentUserDefault())
@@ -41,11 +50,11 @@ class StudentSerializer(serializers.ModelSerializer):
 class UserSerializer(serializers.ModelSerializer):
     profile = StudentSerializer(required=False, read_only=True)
     courses = EnrollmentSerializer(source="enrollment_set", many=True, read_only=True)
-    # day_entries = DayEntrySerializer(many=True, read_only=True)
+    day_entries = DayEntrySerializer(many=True, read_only=True)
 
     class Meta:
         model = User
-        fields = ['url', 'email', 'name', 'username', 'password', 'profile', 'courses']
+        fields = ['url', 'email', 'name', 'username', 'password', 'profile', 'courses', 'day_entries']
         extra_kwargs = {
             'password': {'write_only': True}
         }
@@ -57,13 +66,47 @@ class UserSerializer(serializers.ModelSerializer):
 
 
 class AssignmentSerializer(serializers.ModelSerializer):
+    avg_total_minutes = serializers.SerializerMethodField()
+    avg_daily_minutes = serializers.SerializerMethodField()
+
     class Meta:
         model = Assignment
-        fields = ['assigned_date', 'due_date', 'course', 'type', 'number']
+        fields = ['assigned_date', 'due_date', 'course', 'type', 'number', 'avg_total_minutes', 
+            'avg_daily_minutes']
+    
+    def get_avg_total_minutes(self, obj):
+        day_entries = obj.day_entries
+        enrollment_size = obj.course.student.count()
+        duration = timedelta(minutes=0)
+        for day_entry in day_entries.all(): 
+            duration += day_entry.duration
+        return str(duration / enrollment_size)
+
+    def get_avg_daily_minutes(self, obj):
+        day_entries = obj.day_entries
+        enrollment_size = obj.course.student.count()
+        print(enrollment_size)
+        duration_dict = {} 
+        for day_entry in day_entries.all(): 
+            day = day_entry.date
+            if day not in duration_dict: 
+                duration_dict[day] = day_entry.duration
+            else: 
+                duration_dict[day] += day_entry.duration
+        date_range = duration_dict.keys()
+        stringified_duration_dict = {}
+        for day in daterange(min([min(date_range), obj.assigned_date]), max([max(date_range), obj.due_date])):
+            if day not in duration_dict:
+                stringified_duration_dict[str(day)] = timedelta(hours=0)
+            else:
+                stringified_duration_dict[str(day)] = str(duration_dict[day] / enrollment_size)
+        return stringified_duration_dict
+        
 
 
 class CourseSerializer(serializers.ModelSerializer):
     assignments = AssignmentSerializer(many=True, read_only=True)
+    
 
     class Meta:
         model = Course
@@ -73,11 +116,14 @@ class CourseSerializer(serializers.ModelSerializer):
 
 class StatisticsSerializer(serializers.Serializer):
     total_minutes_this_week = serializers.SerializerMethodField()
+    
 
     def get_total_minutes_this_week(self, obj):
         today = obj
         closest_sunday = today - datetime.timedelta(days=today.weekday())
-        relevant_entries = self.context['request'].user.day_entries.filter(date__gte=closest_sunday, date__lte=today)
+        next_sunday = closest_sunday + datetime.timedelta(days=7)
+        relevant_entries = self.context['request'].user.day_entries.filter(
+            date__gte=closest_sunday, date__lte=next_sunday)
         # print(relevant_entries)
         total_hour_count = timedelta(0)
         for entry in relevant_entries: 
